0ed2aed74a457fa918e1cc6a0fee84edab201bf2
Fluvia Antony <20079668@mydbs.ie>
2025-11-17 20:43:39 +0000

    User registration and login | Uses MySQL queries to read/write user data"

---
+++ routes/userRoutes.js
@@ -0,0 +1,40 @@
+const express = require("express"); //express  the web framework used to handle http request
+const router = express.Router();//a mini-router object to define endpoints(like /signup and login)
+//this router will be connected to the main Express app.
+const db = require("../db"); //db: databse connection module// run sql queries
+
+// Signup : Handles POST requests sent to /signup // create new user in usert able
+router.post("/signup", (req, res) => {
+  const { name, email, password } = req.body; // Contains the data sent from the client (frontend)
+
+  db.query(
+    "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+    [name, email, password],
+    (err) => {
+      if (err) return res.json({ status: "error", error: err });
+      res.json({ status: "success", message: "User created" });
+    }
+  );
+});
+
+// Login. // Handles POST requests sent to /login.
+router.post("/login", (req, res) => 
+    {
+    //It checks if there is a user with the given email and password.
+  const { email, password } = req.body;
+
+  db.query(
+    "SELECT * FROM users WHERE email = ? AND password = ?",
+    [email, password],
+    (err, results) => {
+      if (err) return res.json({ status: "error", error: err });
+
+      if (results.length === 0)
+        return res.json({ status: "fail", message: "Invalid credentials" });
+
+      res.json({ status: "success", user: results[0] });
+    }
+  );
+});
+
+module.exports = router;
