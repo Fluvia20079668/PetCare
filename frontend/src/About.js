0b55a13237cce8ba724e75c7a391d3d547280fab
Fluvia Antony <20079668@mydbs.ie>
2025-11-18 23:17:10 +0000

    Aboutpage created

---
+++ frontend/src/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Us</h1>
+      <p>We provide the best pet daycare and hostel services for your furry friends.</p>
+    </div>
+  );
+}
