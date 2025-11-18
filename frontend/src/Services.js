0492539ef04a9ef1a3986ad4adc727c53bfdf80f
Fluvia Antony <20079668@mydbs.ie>
2025-11-18 23:19:41 +0000

    servicepage created

---
+++ frontend/src/Services.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Services() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Our Services</h1>
+      <ul>
+        <li>Pet Daycare</li>
+        <li>Pet Hostel</li>
+        <li>Grooming</li>
+        <li>Training</li>
+      </ul>
+    </div>
+  );
+}
