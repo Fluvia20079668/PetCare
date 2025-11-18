ca4912c225ef9ccff4d5208d5332fa26c8ee0cc5
Fluvia Antony <20079668@mydbs.ie>
2025-11-18 23:18:38 +0000

    Contactpage created

---
+++ frontend/src/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Contact Us</h1>
+      <p>Email: info@petcare.com</p>
+      <p>Phone: +123 456 7890</p>
+    </div>
+  );
+}
