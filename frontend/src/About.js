0ff4938bd43af6b5117ff480f8a42f59d5aa4b8c
Fluvia Antony <20079668@mydbs.ie>
2025-11-19 20:53:34 +0000

    reworked app.js

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
+import Booking from "./Booking";
 
-export default function About() {
+function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>About Us</h1>
-      <p>We provide the best pet daycare and hostel services for your furry friends.</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* Login/Signup */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Booking page (protected) */}
+        <Route path="/booking" element={<Booking />} />
+
+        {/* fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
