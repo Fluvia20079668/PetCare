9ee2d6b8ed52fe97f6755e58538701e15ba9ec52
Fluvia Antony <20079668@mydbs.ie>
2025-11-23 00:05:45 +0000

    resolved navigation issue in home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -78,10 +78,10 @@ export default function Home() {
       <nav className="pc-nav">
         <div className="pc-logo">🐾 PetCare+</div>
         <ul className="pc-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
         <div className="pc-auth">
           <button className="btn-outline" onClick={() => navigate("/login")}>Login</button>
