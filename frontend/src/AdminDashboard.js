579b89e97cc745f7e3f012220ffda0ee160aff76
Fluvia Antony <20079668@mydbs.ie>
2025-12-09 16:20:42 +0000

    deplyoing the webpage to netify

--- frontend/src/AdminDashboard.js
+++ frontend/src/AdminDashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,fetchBookings,fetchUsers }from "react";
 import axios from "axios";
 import "./AdminDashboard.css";
 
